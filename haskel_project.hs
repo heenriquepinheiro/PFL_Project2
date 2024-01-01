0105c5f64676a419c30dce05fdb4963aedcca455
francis802 <franciscosccampos@gmail.com>
2024-01-01 22:28:58 +0000

    Almost everything done

--- haskel_project.hs
+++ haskel_project.hs
@@ -181,16 +181,17 @@ testAssembler :: Code -> (String, String)
 testAssembler code = (stack2Str stack, state2Str state)
   where (_,stack,state) = run(code, createEmptyStack, createEmptyState)
 
--- Examples:
--- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
--- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
--- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
--- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
--- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
--- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
--- testAssembler [Push (-20),Push (-21), Le] == ("True","")
--- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
--- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
+testCaseAssembler :: IO ()
+testCaseAssembler = do
+  print $ testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+  print $ testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
+  print $ testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
+  print $ testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
+  print $ testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
+  print $ testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
+  print $ testAssembler [Push (-20),Push (-21), Le] == ("True","")
+  print $ testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+  print $ testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
 -- If you test:
 -- testAssembler [Push 1,Push 2,And]
 -- You should get an exception with the string: "Run-time error"
@@ -441,24 +442,22 @@ parseAndOrEqBOrNotOrEqAOrLeOrBoolOrPar tokens =
     result -> result
 
 
--------- STATEMENT PARSER --------
+-------- STATEMENTS PARSER --------
 parseStatements :: [Token] -> Maybe (Program, [Token])
 parseStatements [] = Just ([], [])
+parseStatements (OpenP : restTokens1) =
+  case parseStatements restTokens1 of
+    Just (stmts, (CloseP : restTokens2)) -> Just (stmts, restTokens2)
+    _ -> Nothing -- no closing paren
 parseStatements tokens =
   case parseStatement tokens of
     Just (stmt, restTokens) ->
-      case parseStatementParen restTokens of
+      case parseStatements restTokens of
         Just (stmts, finalRestTokens) -> Just (stmt : stmts, finalRestTokens)
         _ -> Just ([stmt], restTokens)
     Nothing -> Nothing
 
-parseStatementParen :: [Token] -> Maybe (Program, [Token])
-parseStatementParen (OpenP : restTokens1) =
-  case parseStatements restTokens1 of
-    Just (stmts, (CloseP : restTokens2)) -> Just (stmts, restTokens2)
-    _ -> Nothing -- no closing paren
-parseStatementParen tokens = parseStatements tokens
-
+-------------- ASSIGNMENT STATEMENT PARSER --------------
 parseStatement :: [Token] -> Maybe (Stm, [Token])
 parseStatement (VarTok var : AssignTok : restTokens1) =
   case parseSumOrProdOrIntOrPar restTokens1 of
@@ -466,19 +465,6 @@ parseStatement (VarTok var : AssignTok : restTokens1) =
       Just (Assignment var expr, restTokens2)
     _ -> Nothing
 
-{-
-parseStatement :: [Token] -> Maybe (Program, [Token])
-parseStatement [] = Just ([], [])
-parseStatement (VarTok var : AssignTok : restTokens1) =
-  case parseSumOrProdOrIntOrPar restTokens1 of
-    Just (expr, SemicolonTok : restTokens2) ->
-      case parseStatement restTokens2 of
-        Just (stmts, restTokens3) ->
-          Just ([Assignment var expr] ++ stmts, restTokens3) -- CURRENTLY NOT WORKING
-        Nothing ->
-          Just ([Assignment var expr], restTokens2)
-    _ -> Nothing
--}
 ------------------ IF STATEMENT PARSER -----------------
 parseStatement (IfTok : restTokens1) =
   case parseAndOrEqBOrNotOrEqAOrLeOrBoolOrPar restTokens1 of
@@ -526,9 +512,6 @@ parseElseStatements tokens =
         Just (stmt, restTokens) -> Just ([stmt], restTokens)
         _ -> Nothing
 
-
-
-
 parser :: [Token] -> Program
 parser tokens =
   case parseStatements tokens of
@@ -545,23 +528,19 @@ testParser :: String -> (String, String)
 testParser programCode = (stack2Str stack, state2Str store)
   where (_,stack,store) = run(compile (parse programCode), createEmptyStack, createEmptyState)
 
--- Examples:
--- testParser "x := 5; x := x - 1;" == ("","x=4")
--- testParser "x := 0 - 2;" == ("","x=-2")
--- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
--- testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
--- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
--- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
--- testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
--- testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
--- testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
--- testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
--- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
--- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+testCaseParser :: IO ()
+testCaseParser = do
+  -- Test Parser
+  print $ testParser "x := 5; x := x - 1;" == ("","x=4")
+  print $ testParser "x := 0 - 2;" == ("","x=-2")
+  print $ testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
+  print $ testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
+  print $ testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
+  print $ testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
+  print $ testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
+  print $ testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
+  print $ testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
 
-main :: IO ()
-main = do
-  putStrLn "Hello, World!"
-  putStrLn "This is a Haskell program."
-  putStrLn "It doesn't do much, but it's a start."
-  putStrLn "Have fun!"
